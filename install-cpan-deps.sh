2f2c245b85207952902a3080b234c9e570b7c2a7
Mason James <mtj@kohaaloha.com>
2013-07-23 22:59:02 +1200

    modified:   .travis.yml 	modified:   install-cpan-deps.sh

--- install-cpan-deps.sh
+++ install-cpan-deps.sh
@@ -1,3 +1,4 @@
 # apt-get install libmodern-perl-perl
 # cat ./perl-deps | cpanm  --installdeps --notest
-cat ./cpan-deps | cpanm  -S --installdeps 
+#cat ./cpan-deps | cpanm  -S --installdeps 
+cat ./cpan-deps | cpanm  --installdeps 
